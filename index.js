aae288fee556ac39f82169aadf2f7ead4436d9f9
ritikadhiman <ritikadhimanritz@gmail.com>
2021-05-06 18:09:45 +0530

    cors access control added

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ app.use(
     extended: true,
   })
 );
-app.use(cors());
+// app.use(cors());
+app.use(cors(), function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/", router);
 
 app.get("/", (req, res) => {
